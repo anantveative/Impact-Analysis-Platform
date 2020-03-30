6ac372085feb75ed07e94567f3bcaa41ec18d467
Veative Labs <47560989+Veativetech@users.noreply.github.com>
2020-03-30 14:23:13 +0530

    Update local.php

--- config/autoload/production/local.php
+++ config/autoload/production/local.php
@@ -4,7 +4,7 @@ return array(
     'db' => array(
         // for primary db adapter that called
         'username' => 'root',
-        'password' => 'root',
+        'password' => '',
     ),
     'zf-mvc-auth' => array(
         'authentication' => array(
